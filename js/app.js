69c94eabb3bac41c02e4dd32227bff08fc8a47db
Helder <helder.correia@mrm-meteorite.com>
2015-05-14 18:18:44 +0100

    added text shadow, and change the text shadow colour on the span randomly

--- js/app.js
+++ js/app.js
@@ -1,1 +1,28 @@
-console.log('app.js loaded!');
+$(function () {
+
+
+	var interval,
+		$span = $('.punkbit header p span');
+
+	function setColour() {
+
+		console.log('setColour');
+
+		interval = setTimeout(function () {
+
+			clearInterval(interval);
+
+			var hex = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
+
+			// change colour
+			$span.css('text-shadow', '0px 0px 11px ' + hex)
+
+			setColour();
+
+		}, 300);
+
+	}
+
+	setColour();
+
+});
